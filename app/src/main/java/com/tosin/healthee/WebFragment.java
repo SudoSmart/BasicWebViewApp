2845010dae2f08128dfb964f0da55c7a7b88f080
nullskull <helloshivampsd@gmail.com>
2019-11-09 18:50:49 +0530

    Sends implicit intent. Gotta create explicit ones.

---
+++ app/src/main/java/com/tosin/healthee/WebFragment.java
@@ -0,0 +1,36 @@
+package com.tosin.healthee;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WebFragment extends Fragment {
+
+    private WebViewModel mViewModel;
+
+    public static WebFragment newInstance() {
+        return new WebFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.web_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(WebViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/tosin/healthee/WebViewModel.java
@@ -0,0 +1,7 @@
+package com.tosin.healthee;
+
+import androidx.lifecycle.ViewModel;
+
+public class WebViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
